b83fd0112cf3dc0ef130b95be4125f87663dd094
chlin61 <chlin61@gmail.com>
2019-12-23 11:55:45 +0800

    add app.py update

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=event.message.text)) # 回復使用者傳來的訊息  就回傳什麼
 
 
 if __name__ == "__main__":
